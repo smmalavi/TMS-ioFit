093866279bc428e6194d163f9130323fafbabf80
Seyed Mohammad Mahdi Alavi <46087039+smmalavi@users.noreply.github.com>
2020-01-15 18:12:00 -0500

    Delete Readme.m

--- Readme.m
+++
@@ -1,8 +0,0 @@
-% Run get_data.m to get the parameters of the model
-
-% then by using the following command, you get the MEP for a given x
-MEP=virtualsubjectEIVStimulate_01(x, myp)
-
-
-
-myp=[-5.0492  -2.6936   1.7746e+06    4.2769   36.4948    0.0924    4.7736   1.5231e-06    0.1040   -6.3927]
